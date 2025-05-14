b76bc40316c4e341f4159bd7ef3161fe01f99762
nimamesbah <nimamesbah85@gmail.com>
2025-05-15 00:12:19 +0430

    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import { useRef, useState } from 'react'
 
 
 function App() {
-  const [todos  , setTodo] = useState([])
+  const [todos  , setTodo] = useState(JSON.parse(localStorage.getItem("todos"))||[])
   let [inEdit ,setInEdit]= useState(null)
     
     function AddTodo(){
         const val = document.querySelector("input")
         if(val.value!=="")
           setTodo([...todos,{id:`${Math.random()*100}`,text:val.value,isDone:false}])
+        setTodo(prev=>{ 
+          localStorage.setItem("todos",JSON.stringify(prev))
+          return prev
+        })
 
         
 
@@ -18,13 +22,20 @@ function App() {
 
       }
     function isDoneHandle(event){
+      setTodo(todos.map(todo => 
+        todo.id === event.target.id ? {...todo, isDone: !todo.isDone} : todo
+      ));
       
-      const element =todos.find(todo=> todo.id===event.target.id)
-      if(element.isDone===false)
-        element.isDone=true
-      else
-        element.isDone=false
-      event.target.checked=element.isDone
+      // const element =todos.find(todo=> todo.id===event.target.id)
+      // if(element.isDone===false)
+      //   element.isDone=true
+      // else
+      //   element.isDone=false
+      // event.target.checked=element.isDone
+      setTodo(prev=>{ 
+        localStorage.setItem("todos",JSON.stringify(prev))
+        return prev
+      })
     
     }
     function editHandle(event,id){
@@ -39,6 +50,10 @@ function App() {
         result.text=inVal
 
       }
+      setTodo(prev=>{ 
+        localStorage.setItem("todos",JSON.stringify(prev))
+        return prev
+      })
 
     }
     function deleteItem(id){
@@ -47,23 +62,29 @@ function App() {
       // console.log("ind",index)
       setTodo(prev => prev.filter(todo => todo.id !==id))
       // setTodo(prev => prev.splice(index,1))
+      setTodo(prev=>{ 
+        localStorage.setItem("todos",JSON.stringify(prev))
+        return prev
+      })
       
     }
+    console.log(todos)
 
     
   
   return (
     <>
       
-      <input type="text"  className='w-36 bg-amber-200 mx-auto mt-4 ' />
-      <button onClick={AddTodo} className='ml-5'>add Todo</button>
-      <ul>
+      <input type="text"  className='w-36 bg-amber-200 mx-auto mt-4 ml-4 rounded-2xl' />
+      <button onClick={AddTodo} className='ml-5 border py-1.5 px-3 rounded-2xl cursor-pointer'>add Todo</button>
+
+      <ul className='ml-4 '>
         {todos.map(todo=> 
-        <li id={todo.id}>
-          {inEdit!==todo.id ? <h1 id={`text${todo.id}`} className='inline mr-3'>{todo.text}</h1> : <input id={`inEdit${todo.id}`} className='border ' type='text' placeholder={todo.text}/>}
-          <input id={todo.id} onChange={isDoneHandle}  type="checkbox"   />
-          <button id={todo.id} className='ml-3 border px-2.5' onClick={()=>editHandle(event,todo.id)}>{inEdit===todo.id?"save":"edit"}</button>
-          <button onClick={()=>deleteItem(todo.id)}>delete</button>
+        <li id={todo.id} className='mt-4 flex gap-2  border py-2 duration-300 px-2.5 w-max rounded-2xl'>
+          {inEdit!==todo.id ? <h1 id={`text${todo.id}`} className=''>{todo.text}</h1> : <input id={`inEdit${todo.id}`} className='border rounded-2xl px-2 ' type='text' placeholder={todo.text}/>}
+          <input id={todo.id} onChange={isDoneHandle}  type="checkbox" checked={todo.isDone}  />
+          <button id={todo.id} className='cursor-pointer' onClick={()=>editHandle(event,todo.id)}>{inEdit===todo.id?"save":"edit"}</button>
+          <button className='border-x px-2 cursor-pointer h-full' onClick={()=>deleteItem(todo.id)}>delete</button>
         </li>    
         )} 
       </ul>
